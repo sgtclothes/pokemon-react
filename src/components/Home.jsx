46e681f2329c2e47b54ed4efd6826dba5bf16f59
Sigit Sasongko <siko.spade31@gmail.com>
2024-09-13 17:16:43 +0700

    configure vercel.json

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import NavigationBar from "./NavigationBar";
 import Carousel from "./Carousel";
 import PokemonList from "./PokemonList";
 
-(async()=> {
-    let response = await fetch("http://103.186.1.188:3007/product/category/cactus?page=1");
+(async () => {
+    let response = await fetch("/api/category/cactus?page=1");
     let json = await response.json();
     console.log(json);
 })();
