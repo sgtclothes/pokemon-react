273178f136ad2834d92286e4ceb2d21979d9b2d5
Sigit Sasongko <siko.spade31@gmail.com>
2024-09-24 15:39:43 +0700

    remove fetch test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,6 @@ import NavigationBar from "./NavigationBar";
 import Carousel from "./Carousel";
 import PokemonList from "./PokemonList";
 
-(async()=> {
-    let response = await fetch("http://103.186.1.188:3007/product/category/cactus?page=1");
-    let json = await response.json();
-    console.log(json);
-})();
-
 function Header() {
     return (
         <>
