215d4d6d136bdd60e6331041bc6350ad3534131b
Sigit Sasongko <siko.spade31@gmail.com>
2024-09-24 17:02:08 +0700

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: "localhost",
-        },
-    },
-    build: {
-        outDir: "dist",
-    },
-    preview: {
-        port: 3000
-    },
 });
